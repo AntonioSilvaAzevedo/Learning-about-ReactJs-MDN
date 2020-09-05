6b817c80f6db24a7df99e4bf115bfd42e4084a71
Antonio Carlos <antonie.dev@gmail.com>
2020-09-05 15:20:00 -0300

    adicionando componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
+import Form from './components/Form'
+import FilterButton from './components/FilterButton'
 
-function App() {
+function App(props) {
+  const tasklist = props.tasks.map(task => (
+    <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed} 
+    key={task.id}
+    />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <Form />
+    <h1>TodoMatic</h1>
+    <div className="filters btn-group stack-exception">
+      <FilterButton />
+      <FilterButton />
+      <FilterButton />
     </div>
-  );
+    <h2 id="list-heading">
+      3 tasks remaining
+    </h2>
+    <ul
+      role="list"
+      className="todo-list stack-large stack-exception"
+      aria-labelledby="list-heading"
+    >
+      {tasklist}      
+    </ul>
+  </div>
+);
 }
-
 export default App;
